e58538c08b08fc399698021f0bbd75d1a258847e
Prudy8 <DeborahMcDougall10@gmail.com>
2026-02-17 22:11:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,48 @@ with open('./data.json') as f:
 @app.get('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.get('/')
+async def hello_world():
+    return 'Hello, World!'
+
+@app.get('/students')
+async def get_students(pref=None):
+    if pref:
+        filtered_students = []
+        for student in data:
+            if student['pref'] == pref:
+                filtered_students.append(student)
+        return filtered_students
+    return data
+
+@app.get('/stats')
+async def get_stats():
+    stats = {}
+
+    for student in data:
+        if student['pref'] in stats:
+            stats[student['pref']] += 1
+        else:
+            stats[student['pref']] = 1
+        
+        if student['programme'] in stats:
+            stats[student['programme']] += 1
+        else:
+            stats[student['programme']] = 1
+
+    return stats
+
+def multiReturnFunc(a,b):
+    return a+b, a-b, a*b, a/b
+
+@app.get('/arithmetic/{a}/{b}')
+async def arithmetic(a:float, b:float):
+    s, t, m, d = multiReturnFunc(a,b)
+
+    return {
+        "sum": s,
+        "take_away": t,
+        "multiply": m,
+        "division": d
+    } 
